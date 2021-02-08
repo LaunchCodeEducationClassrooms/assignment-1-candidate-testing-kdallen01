6a100634dbd2eb23ffbacc6db02a292ee0ca4fb4
allenkd <kdallen@accessus.net>
2021-02-08 22:13:56 +0000

    Cleaned up code.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,57 +3,48 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = ""; //I added name
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? "; //I added question
-let correctAnswer = "Sally Ride"; //I added answer
-let candidateAnswer = ""; //I added answer
-let questions= ["Who was the first American woman in space?", "True or false: 5000 meters = 5 kilometers.", "(5 + 3)/2 * 10 =?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the International Space Station (ISS)?"];  //I added array
-let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];  //I added array
-let candidateAnswers = [];  //I added array
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
+let questions= ["Who was the first American woman in space?", "True or false: 5000 meters = 5 kilometers.", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the International Space Station (ISS)?"];
+let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("Please enter your name: "); //I added line
+candidateName = input.question("Please enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-//candidateAnswer = input.question(question); //I added line
-for (i = 0; i < questions.length; i++) {  //I added line
-//console.log(questions[i]);
-  candidateAnswer = input.question(`\n${i + 1}. ${questions[i]} `);  //I added line
-  console.log(`Your answer: ${candidateAnswer}`)  //I added line
-  console.log(`Correct answer: ${correctAnswers[i]}`)  //I added line
+for (i = 0; i < questions.length; i++) {
+  candidateAnswer = input.question(`\n${i + 1}. ${questions[i]} `);
+  console.log(`Your answer: ${candidateAnswer}`);
+  console.log(`Correct answer: ${correctAnswers[i]}`);
 
   if(candidateAnswer.toLowerCase() === correctAnswers[i].toLowerCase()) {
-    candidateAnswers.push(candidateAnswer);  //I added line
+    candidateAnswers.push(candidateAnswer);
     } 
- } //I added line
-//console.log(candidateAnswers);  //I added line
-}  //I added line
+ }
+}
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
- 
- //if (candidateAnswer === correctAnswer) {  //I added line
-  // console.log("Correct!");  //I added line
- //} else {  //I added line
-   //console.log("Incorrect.");  //I added line
- //} //I added line
+
 
   let grade;
 
-  grade = (candidateAnswers.length/correctAnswers.length * 100)
-  console.log(`\n\n>>> Overall Grade: ${grade}% \(${candidateAnswers.length} of ${correctAnswers.length} responses correct) <<<`)
+  grade = (candidateAnswers.length/correctAnswers.length * 100) 
+    console.log(`\n\n>>> Overall Grade: ${grade}% \(${candidateAnswers.length} of ${correctAnswers.length} responses correct) <<<`);
   if (grade >= 80) {
-    console.log(">>> Status: PASS <<<")
+    console.log(">>> Status: PASS <<<");
   } else {
-    console.log(">>> Status: FAIL <<<")
+    console.log(">>> Status: FAIL <<<");
   }
-    
 
   return grade;
 }
@@ -61,10 +52,11 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log(`Welcome, ${candidateName}!`) //I addd line
+  console.log(`Welcome, ${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
